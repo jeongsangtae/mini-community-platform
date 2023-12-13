31c5b310d8470fe249a61cff3d57233d0114d541
jeongsangtae <wjdtkdxo55@naver.com>
2023-12-14 03:58:46 +0900

    Mini Community Platform - form data state raise up (CreatePost, Posts edit)

--- src/components/Posts/CreatePost.jsx
+++ src/components/Posts/CreatePost.jsx
@@ -1,7 +1,81 @@
-const CreatePost = () => {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CreatePost = ({ onAddPost }) => {
+  const [title, setTitle] = useState("");
+  const [name, setName] = useState("");
+  const [content, setContent] = useState("");
+
+  const navigate = useNavigate();
+
+  const changeTitleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const changeNameHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const changeContentHandler = (event) => {
+    setContent(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const postData = {
+      id: Math.random(),
+      title: title,
+      name: name,
+      content: content,
+    };
+
+    onAddPost(postData);
+  };
+
+  const closeHandler = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <p>게시글 추가 페이지</p>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="title">제목</label>
+          <input
+            required
+            type="text"
+            id="title"
+            name="title"
+            onChange={changeTitleHandler}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">작성자</label>
+          <input
+            required
+            type="text"
+            id="name"
+            name="name"
+            onChange={changeNameHandler}
+          />
+        </div>
+        <div>
+          <textarea
+            required
+            name="content"
+            placeholder="내용 입력"
+            onChange={changeContentHandler}
+          />
+        </div>
+        <div>
+          <button type="button" onClick={closeHandler}>
+            Cancel
+          </button>
+          <button>Submit</button>
+        </div>
+      </form>
     </>
   );
 };
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import CreatePost from "./CreatePost";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  const addPostHandler = (postData) => {
+    setPosts((prevPosts) => {
+      return [postData, ...prevPosts];
+    });
+  };
+
   return (
     <>
       <p>게시글~</p>
       <Link to="create-post">게시글 추가</Link>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li>
+              <p>{post.title}</p>
+              <p>{post.name}</p>
+              <p>{post.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <CreatePost onAddPost={addPostHandler} />
     </>
   );
 };
