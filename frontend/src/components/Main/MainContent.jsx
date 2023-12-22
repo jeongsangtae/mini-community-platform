98f0579a3e1f665ca2736dabb0b755a274f46c08
jeongsangtae <wjdtkdxo55@naver.com>
2023-12-23 04:46:40 +0900

    Mini Community Platform - MainContent edit & CSS add

--- frontend/src/components/Main/MainContent.jsx
+++ frontend/src/components/Main/MainContent.jsx
@@ -1,8 +1,10 @@
-const MainContent = ({ title, children }) => {
+import classes from "./MainContent.module.css";
+
+const MainContent = () => {
   return (
-    <div>
-      <h1>{title}</h1>
-      {children}
+    <div className={classes.mainContent}>
+      <h1>미니 커뮤니티 플랫폼</h1>
+      <p>미니 커뮤니티 플랫폼</p>
     </div>
   );
 };
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,7 @@
 import MainContent from "../components/Main/MainContent";
 
 const HomePage = () => {
-  return (
-    <MainContent title="미니 커뮤니티 플랫폼">
-      <p>미니 커뮤니티 플랫폼</p>
-    </MainContent>
-  );
+  return <MainContent />;
 };
 
 export default HomePage;
