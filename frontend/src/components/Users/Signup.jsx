ecdb3245bece7b5f75816175d8b26d89d17f4994
jeongsangtae <wjdtkdxo55@naver.com>
2024-01-14 06:25:13 +0900

    Mini Community Platform - Signup data save logic add

--- frontend/src/components/Users/Signup.jsx
+++ frontend/src/components/Users/Signup.jsx
@@ -1,14 +1,54 @@
+import { useState } from "react";
+
 import Modal from "../UI/Modal";
 import classes from "./Signup.module.css";
 
 const Signup = ({ toggle }) => {
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    confirmEmail: "",
+    password: "",
+  });
+
+  const inputChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setSignupData({ ...signupData, [name]: value });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(signupData);
+
+    setSignupData({
+      name: "",
+      email: "",
+      confirmEmail: "",
+      password: "",
+    });
+
+    // const response = fetch("http://localhost:3000/signup", {
+    //   method: "POST",
+    //   body: JSON.stringify(signData),
+    //   headers: {"Content-Type": "application/json"}
+    // })
+  };
+
   return (
     <Modal onClose={toggle}>
       <p className={classes.heading}>회원가입 페이지</p>
-      <form method="post" className={classes.form}>
+      <form method="post" className={classes.form} onSubmit={submitHandler}>
         <div>
           <label htmlFor="email">이메일</label>
-          <input required type="email" id="email" name="email" />
+          <input
+            required
+            type="email"
+            id="email"
+            name="email"
+            value={signupData.email}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div>
@@ -17,22 +57,38 @@ const Signup = ({ toggle }) => {
             required
             type="email"
             id="confirm-email"
-            name="confirm-email"
+            name="confirmEmail"
+            value={signupData.confirmEmail}
+            onChange={inputChangeHandler}
           />
         </div>
 
         <div>
           <label htmlFor="name">이름</label>
-          <input required type="text" id="name" name="name" />
+          <input
+            required
+            type="text"
+            id="name"
+            name="name"
+            value={signupData.name}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div>
           <label htmlFor="password">비밀번호</label>
-          <input required type="password" id="password" name="password" />
+          <input
+            required
+            type="password"
+            id="password"
+            name="password"
+            value={signupData.password}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div className={classes.actions}>
-          <button>가입</button>
+          <button type="submit">가입</button>
           <button onClick={toggle}>취소</button>
         </div>
         <a href="/login" className={classes.login}>
