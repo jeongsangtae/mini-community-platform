c1f34ba2b60d221393a9f9c6454e54ecded6cf0c
jeongsangtae <wjdtkdxo55@naver.com>
2024-01-17 15:53:29 +0900

    Mini Community Platform - Login data save logic add (frontend code)

--- frontend/src/components/Users/Login.jsx
+++ frontend/src/components/Users/Login.jsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Login.module.css";
 
 const Login = ({ toggle }) => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const inputChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(loginData);
+  };
+
   return (
     <Modal onClose={toggle}>
       <p className={classes.heading}>로그인 페이지</p>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div>
           <label htmlFor="email">이메일</label>
-          <input required type="text" id="email" name="email" />
+          <input
+            required
+            type="email"
+            id="email"
+            name="email"
+            value={loginData.email}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div>
           <label htmlFor="password">비밀번호</label>
-          <input required type="password" id="password" name="password" />
+          <input
+            required
+            type="password"
+            id="password"
+            name="password"
+            value={loginData.password}
+            onChange={inputChangeHandler}
+          />
         </div>
 
         <div className={classes.actions}>
-          <button>가입</button>
+          <button type="submit">가입</button>
           <button onClick={toggle}>취소</button>
         </div>
         <a href="/signup" className={classes.signup}>
