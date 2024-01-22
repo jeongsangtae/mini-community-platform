67c6467d38cb4651ccced6f2a6e3a495625b33d5
jeongsangtae <wjdtkdxo55@naver.com>
2024-01-22 22:57:05 +0900

    Mini Community Platform - jsonwebtoken library add & jwt-auth logic add

---
+++ backend/middlewares/jwt-auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const jwtAuth = (req, res, next) => {
+  const token = req.header("Authorization");
+  const tokenKey = process.env.TOKEN_KEY;
+
+  if (!token) {
+    return res.status(401).json({ message: "토큰이 없습니다." });
+  }
+
+  jwt.verify(token, tokenKey, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "토큰 검증 실패" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = jwtAuth;
