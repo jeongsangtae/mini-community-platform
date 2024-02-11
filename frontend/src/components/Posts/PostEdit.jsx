70f369730b03e4f737b1b694d42e2a0ff23f8a2a
jeongsangtae <wjdtkdxo55@naver.com>
2024-02-11 22:16:22 +0900

    Mini Community Platform - Display post content on post edit page

--- frontend/src/components/Posts/PostEdit.jsx
+++ frontend/src/components/Posts/PostEdit.jsx
@@ -1,5 +1,53 @@
+import { useRouteLoaderData, useNavigate, Form } from "react-router-dom";
+
+import classes from "./PostEdit.module.css";
+
 const PostEdit = () => {
-  return <>게시글 수정 페이지</>;
+  const post = useRouteLoaderData("post-detail");
+
+  console.log(post);
+
+  const navigate = useNavigate();
+
+  const closeHandler = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <h1 className={classes.heading}>게시글 수정 페이지</h1>
+      <Form method="post" className={classes.form}>
+        <div>
+          <label htmlFor="title">제목</label>
+          <input
+            required
+            type="text"
+            id="title"
+            name="title"
+            value={post.title}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">작성자</label>
+          <input required type="text" id="name" name="name" value={post.name} />
+        </div>
+        <div>
+          <textarea
+            required
+            name="content"
+            placeholder="내용 입력"
+            value={post.content}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button type="button" onClick={closeHandler}>
+            취소
+          </button>
+          <button>등록</button>
+        </div>
+      </Form>
+    </>
+  );
 };
 
 export default PostEdit;
