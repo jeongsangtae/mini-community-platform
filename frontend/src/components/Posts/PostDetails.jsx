e5acbf31bcd3755e52d413f490db0de12a6f2470
jeongsangtae <wjdtkdxo55@naver.com>
2024-02-18 05:39:05 +0900

    Mini Community Platform - post delete action function add (incomplete)

--- frontend/src/components/Posts/PostDetails.jsx
+++ frontend/src/components/Posts/PostDetails.jsx
@@ -60,20 +60,3 @@ const PostDetails = () => {
 };
 
 export default PostDetails;
-
-export const action = async ({ params }) => {
-  const postId = params.postId;
-  const response = await fetch("http://localhost:3000/posts/" + postId, {
-    method: "DELETE",
-    body: JSON.stringify(),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (!response.ok) {
-    throw json({ message: "Could not delete post." }, { status: 500 });
-  }
-
-  return redirect("/posts");
-};
