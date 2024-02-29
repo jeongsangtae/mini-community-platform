2139b7cc85ecddba7498040e99a267f1773c9f8a
jeongsangtae <wjdtkdxo55@naver.com>
2024-03-01 00:48:38 +0900

    Mini Community Platform - Reply add logic complete

--- frontend/src/components/Replies/ReplyForm.jsx
+++ frontend/src/components/Replies/ReplyForm.jsx
@@ -1,6 +1,43 @@
-const ReplyForm = ({ onReplyToggle }) => {
-  const submitHandler = (event) => {
+import { useState } from "react";
+import { useRouteLoaderData } from "react-router-dom";
+
+const ReplyForm = ({ method, onAddReplyData, onReplyToggle }) => {
+  const [reply, setReply] = useState();
+  const post = useRouteLoaderData("post-detail");
+
+  const replyinputHandler = (event) => {
+    setReply(event.target.value);
+  };
+
+  const submitHandler = async (event) => {
     event.preventDefault();
+
+    const postId = post.postId;
+
+    let requestBody = {
+      content: reply,
+    };
+
+    if (method === "PATCH") {
+      requestBody.commentId = commentData.commentId;
+    }
+
+    const response = await fetch(
+      "http://localhost:3000/posts/" + postId + "/replies",
+      {
+        method: method,
+        body: JSON.stringify(requestBody),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    if (!response.ok) {
+      throw json({ message: "Could not save reply." }, { status: 500 });
+    } else if (response.ok && method === "POST") {
+      const resData = await response.json();
+      onAddReplyData(resData.newReply);
+    }
+    onReplyToggle();
   };
 
   return (
@@ -11,8 +48,8 @@ const ReplyForm = ({ onReplyToggle }) => {
           name="content"
           rows="5"
           placeholder="내용 입력"
-          // defaultValue={commentData ? commentData.content : ""}
-          // onChange={commentInputHandler}
+          // defaultValue={commentReply ? commentReply.content : ""}
+          onChange={replyinputHandler}
         />
         <button>등록</button>
         {onReplyToggle && <button onClick={onReplyToggle}>취소</button>}
