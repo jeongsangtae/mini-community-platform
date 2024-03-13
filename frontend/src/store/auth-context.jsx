3e94523d848fcbed751c3b8a6c04191aecc1d68f
jeongsangtae <wjdtkdxo55@naver.com>
2024-03-14 04:08:04 +0900

    Mini Community Platform - Login localStorage expiration time add

--- frontend/src/store/auth-context.jsx
+++ frontend/src/store/auth-context.jsx
@@ -12,6 +12,17 @@ export const AuthContextProvier = ({ children }) => {
   // const [userData, setUserData] = useState(null);
 
   useEffect(() => {
+    const storedExpirationTime = localStorage.getItem("expirationTime");
+    const now = new Date().getTime();
+
+    if (now > storedExpirationTime) {
+      localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("expirationTime");
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+
     const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
 
     if (storedUserLoggedInInformation === "1") {
@@ -20,12 +31,26 @@ export const AuthContextProvier = ({ children }) => {
   }, []);
 
   const loginHandler = () => {
+    const now = new Date().getTime();
+    const expirationTime = now + 60 * 60 * 1000;
     localStorage.setItem("isLoggedIn", "1");
+    localStorage.setItem("expirationTime", expirationTime);
     setIsLoggedIn(true);
     // setUserData(userData);
+
+    setTimeout(() => {
+      localStorage.removeItem("isLoggedIn");
+      setIsLoggedIn(false);
+    }, 60 * 60 * 1000);
+
+    console.log(now);
+    console.log(expirationTime);
+    console.log(new Date(now).toString());
   };
+
   const logoutHandler = () => {
     localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("expirationTime");
     setIsLoggedIn(false);
     // setUserData(null);
   };
