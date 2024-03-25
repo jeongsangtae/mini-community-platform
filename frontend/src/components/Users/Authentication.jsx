ab580cea73b552895a8fe011375a5130ec74a2d6
jeongsangtae <wjdtkdxo55@naver.com>
2024-03-25 23:52:58 +0900

    Mini Community Platform - Authentication Component Multiple Type Tests

--- frontend/src/components/Users/Authentication.jsx
+++ frontend/src/components/Users/Authentication.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import ErrorPage from "../../pages/ErrorPage";
 
 import AuthContext from "../../store/auth-context";
 
@@ -9,11 +10,35 @@ const Authentication = ({ children }) => {
 
   useEffect(() => {
     if (!authCtx.isLoggedIn) {
-      navigate("/");
+      navigate("/*");
     }
   }, [authCtx.isLoggedIn, navigate]);
 
-  return <>{authCtx.isLoggedIn ? children : null}</>;
+  return <>{authCtx.isLoggedIn ? children : <ErrorPage />}</>;
+
+  // useEffect(() => {
+  //   if (!authCtx.isLoggedIn) {
+  //     navigate("/");
+  //   }
+  // }, [authCtx.isLoggedIn, navigate]);
+
+  // return <>{authCtx.isLoggedIn ? children : <ErrorPage />}</>;
+
+  // useEffect(() => {
+  //   if (!authCtx.isLoggedIn) {
+  //     navigate("/");
+  //   }
+  // }, [authCtx.isLoggedIn, navigate]);
+
+  // return <>{authCtx.isLoggedIn ? children : null}</>;
+
+  // useEffect(() => {
+  //   if (!authCtx.isLoggedIn) {
+  //     navigate(<ErrorPage />);
+  //   }
+  // }, [authCtx.isLoggedIn, navigate]);
+
+  // return <>{authCtx.isLoggedIn ? children : null}</>;
 };
 
 export default Authentication;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    errorElement: <ErrorPage />,
+    // errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
