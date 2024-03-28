09266d8575a0dbd3541af77848774a4be7e82b32
jeongsangtae <wjdtkdxo55@naver.com>
2024-03-28 23:31:06 +0900

    Mini Community Platform - PostEditPage error message conditional add (failed)

--- frontend/src/components/Users/Authentication.jsx
+++ frontend/src/components/Users/Authentication.jsx
@@ -1,12 +1,28 @@
 import { useContext } from "react";
+import { useRouteLoaderData } from "react-router-dom";
 
 import AuthContext from "../../store/auth-context";
 import NoAccess from "./NoAccess";
 
 const Authentication = ({ children }) => {
   const authCtx = useContext(AuthContext);
+  const postData = useRouteLoaderData("post-detail");
 
-  return <>{authCtx.isLoggedIn ? children : <NoAccess />}</>;
+  let content;
+
+  if (authCtx.isLoggedIn) {
+    content = children;
+  } else {
+    content = <NoAccess checkWriter={authCtx.isLoggedIn} />;
+  }
+
+  if (authCtx.isLoggedIn && postData.email === authCtx.userInfo?.email) {
+    content = children;
+  } else {
+    content = <NoAccess checkWriter={authCtx.isLoggedIn} />;
+  }
+
+  return <>{content}</>;
 };
 
 export default Authentication;
--- frontend/src/pages/PostEditPage.jsx
+++ frontend/src/pages/PostEditPage.jsx
@@ -1,16 +1,28 @@
+import { useContext } from "react";
 import { useRouteLoaderData } from "react-router-dom";
+
 import PostForm from "../components/Posts/PostForm";
+import AuthContext from "../store/auth-context";
 
 const PostEditPage = () => {
+  const authCtx = useContext(AuthContext);
   const postData = useRouteLoaderData("post-detail");
+
   console.log(postData);
+
+  // if (postData.email === authCtx.userInfo?.email) {
+
+  // }
+
   return (
     <>
-      <PostForm
-        method="patch"
-        postData={postData}
-        postPageName="게시글 수정 페이지"
-      />
+      {postData.email === authCtx.userInfo?.email && (
+        <PostForm
+          method="patch"
+          postData={postData}
+          postPageName="게시글 수정 페이지"
+        />
+      )}
     </>
   );
 };
