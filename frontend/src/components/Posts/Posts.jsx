128f224630414c01f94d668228ad5593baf30506
jeongsangtae <wjdtkdxo55@naver.com>
2024-04-01 22:30:20 +0900

    Mini Community Platform - React Content Loader add & use

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import { PostsSkeleton } from "../UI/SkeletonUI";
+import { PostsSkeleton } from "../UI/SkeletonUI";
 
 import Post from "./Post";
 import Pagination from "./PagiNation";
@@ -16,7 +16,6 @@ const Posts = () => {
   const [totalPages, setTotalPages] = useState(1);
   const [firstPageGroup, setFirstPageGroup] = useState(1);
   const [lastPageGroup, setLastPageGroup] = useState(1);
-  // const [loggedIn, setLoggedIn] = useState(false);
 
   const fetchData = async (pageNumber) => {
     const response = await fetch(
@@ -44,11 +43,6 @@ const Posts = () => {
     paginationFetchData(page);
   }, [page]);
 
-  // useEffect(() => {
-  //   console.log(authCtx.isLoggedIn);
-  //   setLoggedIn(authCtx.isLoggedIn);
-  // }, [authCtx]);
-
   const postAddButtonClass = authCtx.isLoggedIn
     ? `${classes.add}`
     : `${classes.add} ${classes.opacity}`;
@@ -59,38 +53,44 @@ const Posts = () => {
 
   return (
     <>
-      <h1 className={classes.heading}>게시글</h1>
-      <div className={classes["posts-item"]}>
-        <p>번호</p>
-        <p>제목</p>
-        <p>글쓴이</p>
-        <p>날짜</p>
-      </div>
-      {posts.length > 0 && (
-        <ul className={classes.posts}>
-          {posts.map((post) => {
-            return (
-              <Post
-                key={post.postId}
-                num={post.postId}
-                title={post.title}
-                name={post.name}
-                date={post.date}
-              />
-            );
-          })}
-        </ul>
+      {!authCtx.isLoggedIn ? (
+        <PostsSkeleton />
+      ) : (
+        <>
+          <h1 className={classes.heading}>게시글</h1>
+          <div className={classes["posts-item"]}>
+            <p>번호</p>
+            <p>제목</p>
+            <p>글쓴이</p>
+            <p>날짜</p>
+          </div>
+          {posts.length > 0 && (
+            <ul className={classes.posts}>
+              {posts.map((post) => {
+                return (
+                  <Post
+                    key={post.postId}
+                    num={post.postId}
+                    title={post.title}
+                    name={post.name}
+                    date={post.date}
+                  />
+                );
+              })}
+            </ul>
+          )}
+          <Pagination
+            page={page}
+            totalPages={totalPages}
+            firstPageGroup={firstPageGroup}
+            lastPageGroup={lastPageGroup}
+            onPageChange={onPageChange}
+          />
+          <Link to="create-post" className={postAddButtonClass}>
+            <p>게시글 추가</p>
+          </Link>
+        </>
       )}
-      <Pagination
-        page={page}
-        totalPages={totalPages}
-        firstPageGroup={firstPageGroup}
-        lastPageGroup={lastPageGroup}
-        onPageChange={onPageChange}
-      />
-      <Link to="create-post" className={postAddButtonClass}>
-        <p>게시글 추가</p>
-      </Link>
     </>
   );
 };
