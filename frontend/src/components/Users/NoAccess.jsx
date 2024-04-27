8a11068e3984158406a5c6075a14c36ff390e610
jeongsangtae <wjdtkdxo55@naver.com>
2024-04-28 04:10:09 +0900

    Mini Community Platform - NoAccess, NotFound CSS edit

--- frontend/src/components/Users/NoAccess.jsx
+++ frontend/src/components/Users/NoAccess.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+
+import AuthContext from "../../store/auth-context";
 import classes from "./NoAccess.module.css";
+
 const NoAccess = ({ message }) => {
+  const authCtx = useContext(AuthContext);
+
   return (
-    <div className={classes["no-access"]}>
+    <div className={`${classes["no-access"]} ${classes[authCtx.themeClass]}`}>
       <h1>{message.title}</h1>
       <p>{message.description}</p>
     </div>
--- frontend/src/components/Users/NotFound.jsx
+++ frontend/src/components/Users/NotFound.jsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import AuthContext from "../../store/auth-context";
 import classes from "./NotFound.module.css";
 
 const NotFound = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
-    <div className={classes["not-found"]}>
+    <div className={`${classes["not-found"]} ${classes[authCtx.themeClass]}`}>
       <h1>이 리소스를 찾을 수 없습니다</h1>
       <p>안타깝게도 이 리소스를 찾을 수 없습니다.</p>
-      <Link to="/" className={classes["redirect-button"]}>
+      <Link
+        to="/"
+        className={`${classes["redirect-button"]} ${
+          classes[authCtx.themeClass]
+        }`}
+      >
         <button>홈으로 돌아가기</button>
       </Link>
     </div>
