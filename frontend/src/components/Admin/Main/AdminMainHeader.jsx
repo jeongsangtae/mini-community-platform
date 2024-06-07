de2de4a05d5afb9387bfb31ef286a14ec9dd370f
jeongsangtae <wjdtkdxo55@naver.com>
2024-06-08 03:16:19 +0900

    Mini Community Platform - Admin dropdown menu component separate (AdminDropDownMenu add)

--- frontend/src/components/Admin/Main/AdminMainHeader.jsx
+++ frontend/src/components/Admin/Main/AdminMainHeader.jsx
@@ -1,10 +1,9 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { LuLogOut } from "react-icons/lu";
-import { User } from "react-feather";
 
 import AuthContext from "../../../store/auth-context";
 import AdminChat from "../Chat/AdminChat";
+import AdminDropDownMenu from "./AdminDropDownMenu";
 import classes from "./AdminMainHeader.module.css";
 
 const AdminMainHeader = () => {
@@ -15,6 +14,10 @@ const AdminMainHeader = () => {
       ? `${classes.active} ${classes.button} ${classes[authCtx.themeClass]}`
       : `${classes.button} ${classes[authCtx.themeClass]}`;
   };
+
+  const dropDownButton = `${classes.button} ${classes["dropdown-button"]} ${
+    classes[authCtx.themeClass]
+  }`;
 
   return (
     <>
@@ -43,60 +46,7 @@ const AdminMainHeader = () => {
                 </div>
               </NavLink>
 
-              <div className={classes.dropdown}>
-                <div className={classes.iconWrapper}>
-                  <User
-                    className={`${classes.icon} ${classes[authCtx.themeClass]}`}
-                  />
-                  <div
-                    className={`${classes.circle} ${
-                      classes[authCtx.themeClass]
-                    }`}
-                  ></div>
-                </div>
-                <div
-                  className={`${classes["dropdown-content"]} ${
-                    classes[authCtx.themeClass]
-                  }`}
-                >
-                  <button
-                    className={`${classes.button} ${
-                      classes["dropdown-button"]
-                    } ${classes[authCtx.themeClass]}`}
-                    onClick={authCtx.logout}
-                  >
-                    <LuLogOut className={classes["dropdown-icon"]} />
-                    로그아웃
-                  </button>
-                  <p
-                    className={`${classes.underline} ${
-                      classes[authCtx.themeClass]
-                    }`}
-                  ></p>
-                  <div className={classes["toggle-button"]}>
-                    <div
-                      className={`${classes["toggle-mode"]} ${
-                        classes[authCtx.themeClass]
-                      }`}
-                    >
-                      라이트 모드
-                    </div>
-                    <div className={`${classes.toggle} ${classes.normal}`}>
-                      <input
-                        id="normal"
-                        className={classes["normal-check"]}
-                        defaultChecked={authCtx.themeMode === "dark"}
-                        type="checkbox"
-                        onChange={authCtx.themeModeToggle}
-                      />
-                      <label
-                        htmlFor="normal"
-                        className={classes["toggle-item"]}
-                      ></label>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <AdminDropDownMenu dropDownButton={dropDownButton} />
             </nav>
           </>
         )}
