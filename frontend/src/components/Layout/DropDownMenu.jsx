972ab1c12307b1b16e11a19915a1385b557f61a3
jeongsangtae <wjdtkdxo55@naver.com>
2024-07-23 02:13:07 +0900

    Mini Community Platform - DropDownMenu conditional content edit

--- frontend/src/components/Layout/DropDownMenu.jsx
+++ frontend/src/components/Layout/DropDownMenu.jsx
@@ -14,115 +14,80 @@ const DropDownMenu = ({
 }) => {
   const authCtx = useContext(AuthContext);
 
-  return (
+  const dropDownContent = (
     <>
       {authCtx.isLoggedIn ? (
-        <div className={classes.dropdown}>
-          <div className={classes["icon-wrapper"]}>
+        <>
+          {localStorage.getItem("role") === "user" && (
+            <NavLink to="/profile" className={dropDownButtonClassName}>
+              <LuUserCircle2 className={classes["dropdown-icon"]} />
+              {authCtx.userInfo?.name}
+            </NavLink>
+          )}
+          <button className={dropDownButtonClassName} onClick={authCtx.logout}>
+            <LuLogOut className={classes["dropdown-icon"]} />
+            로그아웃
+          </button>
+        </>
+      ) : (
+        <>
+          <button className={dropDownButtonClassName} onClick={onSignupToggle}>
+            <FaRegAddressCard className={classes["dropdown-icon"]} />
+            회원가입
+          </button>
+          <button className={dropDownButtonClassName} onClick={onLoginToggle}>
+            <LuLogIn className={classes["dropdown-icon"]} />
+            로그인
+          </button>
+        </>
+      )}
+      <p className={`${classes.underline} ${classes[authCtx.themeClass]}`}></p>
+      <div className={classes["toggle-button"]}>
+        <div
+          className={`${classes["toggle-mode"]} ${classes[authCtx.themeClass]}`}
+        >
+          라이트 모드
+        </div>
+        <div className={`${classes.toggle} ${classes.normal}`}>
+          <input
+            id="normal"
+            className={classes["normal-check"]}
+            defaultChecked={authCtx.themeMode === "dark"}
+            type="checkbox"
+            onChange={authCtx.themeModeToggle}
+          />
+          <label htmlFor="normal" className={classes["toggle-item"]}></label>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <div className={classes.dropdown}>
+      <div className={classes["icon-wrapper"]}>
+        {authCtx.isLoggedIn ? (
+          <>
             <User
               className={`${classes.icon} ${classes[authCtx.themeClass]}`}
             />
             <div
               className={`${classes.circle} ${classes[authCtx.themeClass]}`}
             ></div>
-          </div>
-          <div
-            className={`${classes["dropdown-content"]} ${
-              classes[authCtx.themeClass]
-            }`}
-          >
-            {localStorage.getItem("role") === "user" && (
-              <NavLink to="/profile" className={dropDownButtonClassName}>
-                <LuUserCircle2 className={classes["dropdown-icon"]} />
-                {authCtx.userInfo?.name}
-              </NavLink>
-            )}
-            <button
-              className={dropDownButtonClassName}
-              onClick={authCtx.logout}
-            >
-              <LuLogOut className={classes["dropdown-icon"]} />
-              로그아웃
-            </button>
-            <p
-              className={`${classes.underline} ${classes[authCtx.themeClass]}`}
-            ></p>
-            <div className={classes["toggle-button"]}>
-              <div
-                className={`${classes["toggle-mode"]} ${
-                  classes[authCtx.themeClass]
-                }`}
-              >
-                라이트 모드
-              </div>
-              <div className={`${classes.toggle} ${classes.normal}`}>
-                <input
-                  id="normal"
-                  className={classes["normal-check"]}
-                  defaultChecked={authCtx.themeMode === "dark"}
-                  type="checkbox"
-                  onChange={authCtx.themeModeToggle}
-                />
-                <label
-                  htmlFor="normal"
-                  className={classes["toggle-item"]}
-                ></label>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className={classes.dropdown}>
-          <div className={classes["icon-wrapper"]}>
-            <MoreVertical
-              className={`${classes.icon} ${classes[authCtx.themeClass]}`}
-            />
-          </div>
-          <div
-            className={`${classes["dropdown-content"]} ${
-              classes[authCtx.themeClass]
-            }`}
-          >
-            <button
-              className={dropDownButtonClassName}
-              onClick={onSignupToggle}
-            >
-              <FaRegAddressCard className={classes["dropdown-icon"]} />
-              회원가입
-            </button>
-            <button className={dropDownButtonClassName} onClick={onLoginToggle}>
-              <LuLogIn className={classes["dropdown-icon"]} />
-              로그인
-            </button>
-            <p
-              className={`${classes.underline} ${classes[authCtx.themeClass]}`}
-            ></p>
-            <div className={classes["toggle-button"]}>
-              <div
-                className={`${classes["toggle-mode"]} ${
-                  classes[authCtx.themeClass]
-                }`}
-              >
-                라이트 모드
-              </div>
-              <div className={`${classes.toggle} ${classes.normal}`}>
-                <input
-                  id="normal"
-                  className={classes["normal-check"]}
-                  defaultChecked={authCtx.themeMode === "dark"}
-                  type="checkbox"
-                  onChange={authCtx.themeModeToggle}
-                />
-                <label
-                  htmlFor="normal"
-                  className={classes["toggle-item"]}
-                ></label>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
+          </>
+        ) : (
+          <MoreVertical
+            className={`${classes.icon} ${classes[authCtx.themeClass]}`}
+          />
+        )}
+      </div>
+      <div
+        className={`${classes["dropdown-content"]} ${
+          classes[authCtx.themeClass]
+        }`}
+      >
+        {dropDownContent}
+      </div>
+    </div>
   );
 };
 
