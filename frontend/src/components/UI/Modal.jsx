63633bfd4d69d0052e0f78f3d3d54c62bbb31f46
jeongsangtae <wjdtkdxo55@naver.com>
2024-09-20 02:04:01 +0900

    Mini Community Platform - dropdown menu mode button CSS edit & modal dark mode add & CSS edit

--- frontend/src/components/UI/Modal.jsx
+++ frontend/src/components/UI/Modal.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+
+import UIContext from "../../store/ui-context";
+
 import classes from "./Modal.module.css";
 
 const Modal = ({ children, onClose }) => {
+  const uiCtx = useContext(UIContext);
+
   return (
     <>
       <div className={classes.backdrop} onClick={onClose} />
-      <dialog open className={classes.modal}>
+      <dialog open className={`${classes.modal} ${classes[uiCtx.themeClass]}`}>
         {children}
       </dialog>
     </>
