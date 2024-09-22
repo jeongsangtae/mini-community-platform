91d1c29dea8c1d9fe792ebc480bdef189f8f8905
jeongsangtae <wjdtkdxo55@naver.com>
2024-09-23 03:10:35 +0900

    Mini Community Platform - frontend Pagination name fixed (2/2)

--- frontend/src/components/Users/Profile.jsx
+++ frontend/src/components/Users/Profile.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import Post from "../Posts/Post";
-import Pagination from "../Posts/PagiNation";
+import Pagination from "../Posts/Pagination";
 import Search from "../Posts/Search";
 import LoadingIndicator from "../UI/LoadingIndicator";
 
