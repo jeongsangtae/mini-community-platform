2518200a2e9f6a7934f06a7e6371f93f49baf5ec
jeongsangtae <wjdtkdxo55@naver.com>
2024-10-13 21:09:59 +0900

    Mini Community Platform - mobile environment scroll error test

--- frontend/src/components/UI/Modal.jsx
+++ frontend/src/components/UI/Modal.jsx
@@ -7,15 +7,15 @@ import classes from "./Modal.module.css";
 const Modal = ({ children, onClose }) => {
   const uiCtx = useContext(UIContext);
 
-  useEffect(() => {
-    // 모달이 열릴 때 body 스크롤을 막음
-    document.body.style.overflow = "hidden";
+  // useEffect(() => {
+  //   // 모달이 열릴 때 body 스크롤을 막음
+  //   document.body.style.overflow = "hidden";
 
-    // 모달이 닫힐 때 body 스크롤을 복구
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
+  //   // 모달이 닫힐 때 body 스크롤을 복구
+  //   return () => {
+  //     document.body.style.overflow = "auto";
+  //   };
+  // }, []);
 
   return (
     <>
