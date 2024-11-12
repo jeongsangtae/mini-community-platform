e806abdf9adb6c8f0b2806b3423272a9c935916f
jeongsangtae <wjdtkdxo55@naver.com>
2024-11-13 03:04:47 +0900

    Mini Community Platform - popup component UI context api add & CSS edit

--- frontend/src/components/UI/Popup.jsx
+++ frontend/src/components/UI/Popup.jsx
@@ -1,7 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import UIContext from "../../store/ui-context";
 import classes from "./Popup.module.css";
 
 const Popup = () => {
+  const uiCtx = useContext(UIContext);
+
   const [popupVisible, setPopupVisible] = useState(false);
 
   useEffect(() => {
@@ -20,14 +24,20 @@ const Popup = () => {
   if (!popupVisible) return null;
 
   return (
-    <div className={classes.popup}>
-      <h2 className={classes["popup-title"]}>관리자 페이지 계정 안내</h2>
-      <div className={classes["popup-content"]}>
+    <div className={`${classes.popup} ${classes[uiCtx.themeClass]}`}>
+      <h2 className={`${classes["popup-title"]} ${classes[uiCtx.themeClass]}`}>
+        관리자 페이지 계정 안내
+      </h2>
+      <div
+        className={`${classes["popup-content"]} ${classes[uiCtx.themeClass]}`}
+      >
         <p>관리자 페이지 테스트를 위한 계정 안내</p>
         <p>아이디: admin@admin.com</p>
         <p>비밀번호: </p>
       </div>
-      <div className={classes["popup-footer"]}>
+      <div
+        className={`${classes["popup-footer"]} ${classes[uiCtx.themeClass]}`}
+      >
         <p>브라우저 종료 시 다시 표시됨</p>
         <button onClick={popupCloseHandler}>닫기</button>
       </div>
